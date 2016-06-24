afd1f9ce8904e63279774d6b9319e0bf7d3ae5c7
danieloh91 <daniel.oh.91@gmail.com>
2016-06-24 01:02:26 -0700

    part 10

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,9 +2,17 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
-if (cardTwo === cardFour) {
-  alert('You found a match!');
+//if (cardTwo === cardFour) {
+  //alert('You found a match!');
+  //}
+//else {
+  //alert('Sorry, try again.');
+  //}
+var gameBoard = document.getElementById('game-board');
+function createBoard() {
+  for (var i = 0; i < gameBoard.length; i ++) {
+    var makeCard = document.createElement('div');
+    makeCard.className = 'card';
+    gameBoard.appendChild(makeCard);
   }
-else {
-  alert('Sorry, try again.');
-  }
+}
